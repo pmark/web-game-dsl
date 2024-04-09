ac1a763fa54f5db6af0e6f2fe46e5b6bb84ed91f
P. Mark Anderson <mark@martianrover.com>
2024-04-09 12:34:34 -0700

    attempting to fix Jest React lint error

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,14 @@
 export default {
-  preset: 'ts-jest/presets/default-esm',
+  preset: "ts-jest/presets/default-esm",
   globals: {},
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1', // Resolve .js extensions to .ts files
+    "^(\\.{1,2}/.*)\\.js$": "$1", // Resolve .js extensions to .ts files
   },
   transform: {
-    '^.+\\.ts$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
+    "^.+\\.ts[x]?$": ["ts-jest", { useESM: true }],
   },
-  extensionsToTreatAsEsm: ['.ts'],
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["<rootDir>/__tests__/**/*.test.ts"],
 };
-
